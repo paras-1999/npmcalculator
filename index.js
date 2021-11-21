1e2fff36353c32256f0b00bcdab457decef380da
paras-1999 <paras.saxena@neosoftmail.com>
2021-11-21 22:48:49 +0530

    second

---
+++ index.js
@@ -0,0 +1,4 @@
+module.exports.add = function (a, b) { console.log(`Addition:  ${a + b}`); }
+module.exports.sub = function (a, b) { console.log(`Subtraction:  ${a - b}`); }
+module.exports.mult = function (a, b) { console.log(`"Multiplication:"  ${a * b}`); }
+module.exports.div = function (a, b) { console.log(`"Division:"  ${a / b}`); }
